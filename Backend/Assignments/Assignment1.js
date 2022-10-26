b0da48f995580e5ddf77b4e38201279fd117cedd
Hayazee <syedahayazeeshan@gmail.com>
2022-10-26 10:35:03 +0500

    Brackets-Internship

---
+++ Backend/Assignments/Assignment1.js
@@ -0,0 +1,14 @@
+// 1) we have an array with some indexes
+var lonely = [4, 7, 9, 3, 9, 4, 7, 19];
+
+// 2) remove the duplicates values including the original one
+// 3) use the filter method for this
+var lonelyNumber = lonely.filter((lonelyNumberPicker) => {
+  return lonely.filter((n) => n === lonelyNumberPicker).length === 1;
+});
+// 4) print the unique / single values in the console by also putting {} besides it
+console.log({ lonelyNumber });
+// that will give us the more detailed result
+
+// test data
+// 4,7,9,3,9,4,7,19
